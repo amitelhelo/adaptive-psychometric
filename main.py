23f25826824f44aac256ecec353d46bce2bf4015
amitelhelo <82733528+amitelhelo@users.noreply.github.com>
2021-04-21 17:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from PsychoProject.PracticeSessions.DecisionsAndTimingPracticeSession import Dec
 # TODO check why the CATPracticeSession import takes so long (about 0.5 seconds)
 
 
-def start_practice_timer():
-    student = Student("Haim Moshe")
+student1 = Student("Haim Moshe")
+
+
+def start_timing_and_decisions_practice(student):
     practice_session = DecisionsAndTimingPracticeSession(student, "general")
     practice_session.initiate_session()
     practice_session.session_results.initiate_results_presentation()
 
 
-def start_practice_cat():
-    student = Student("Haim Moshe")
+def start_cat_practice(student):
     practice_session = CATPracticeSession(student, "geometry")
     practice_session.initiate_session()
     practice_session.session_results.initiate_results_presentation()
 
 
-start_practice_cat()
-start_practice_timer()
+start_cat_practice(student1)
+start_timing_and_decisions_practice(student1)
